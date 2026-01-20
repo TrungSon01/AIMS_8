1bb05f6ed8987111470742dc24dbae21e534d823
Nguyễn Trung Sơn <traidatmaxmanh@gmail.com>
2026-01-20 20:16:11 +0700

    commit 1

---
+++ src/main/java/com/example/AIMSVER2/Aimsver2Application.java
@@ -0,0 +1,13 @@
+package com.example.AIMSVER2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aimsver2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aimsver2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AIMSVER2/Aimsver2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AIMSVER2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aimsver2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
